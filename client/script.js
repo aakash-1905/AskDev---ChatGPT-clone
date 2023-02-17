210e25750eb781cbbe40bfb3fbc71c307a6bde8a
Aakash Kumar <aakashkumar051901@gmail.com>
2023-02-17 14:07:42 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ const handleSubmit = async(e)=>{
   loader(messageDiv);
 
   // fetching bot response
-  const response = await fetch('http://localhost:5000',
+  const response = await fetch('https://askdev.onrender.com',
   {
     method: 'POST',
     headers: {
